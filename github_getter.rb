a090e829b0ce5f7c2b73a24f3783087b80b78263
cgrad01 <cgrad01@gmail.com>
2016-03-06 19:47:22 -0500

    get profiles and filter by locatin

--- github_getter.rb
+++ github_getter.rb
@@ -10,13 +10,35 @@ class GithubGetter
     @username = username
     @password = password
     @args = args
+    @users = []
   end
 
-  def get
-    uri = URI(BASE_URL)
+  def get_users
+    uri = URI(BASE_URL + "/users")
     req = Net::HTTP::Get.new(uri)
     req.basic_auth @username, @password
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
-      http.request(req)}
+      http.request(req).body
+    }
   end
+
+  def get_profiles(users)
+    users.each {|user|
+      uri = URI(BASE_URL + "/users" + "/#{user["login"]}")
+      req = Net::HTTP::Get.new(uri)
+      req.basic_auth @username, @password
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+        @users << JSON.parse(http.request(req).body)
+      }
+    }
+    @users
+  end
+
+  def filter_by_location(location)
+    @users.delete_if{|user|
+      !(user["location"].to_s.upcase.include?(location.upcase))
+    }
+    @users
+  end
+
 end
