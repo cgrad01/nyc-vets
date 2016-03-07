a1b7dc83a1fb8ebde96528bd7df3cf5f0f1498b6
cgrad01 <cgrad01@gmail.com>
2016-03-07 10:54:42 -0500

    edit runner

--- runner.rb
+++ runner.rb
@@ -1,6 +1,4 @@
 require_relative 'github_getter'
 
-test = GithubGetter.new("cgrad01", "ogle9hued")
-users = JSON.parse(test.get_users)
-test.get_profiles(users[0..200])
-p test.filter_by_location("New York")
+test = GithubGetter.new()
+p JSON.parse(test.search_users)
