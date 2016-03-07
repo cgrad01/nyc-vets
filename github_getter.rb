6124b5dfa0f497188948b9f3b29fadc27de0e08f
cgrad01 <cgrad01@gmail.com>
2016-03-07 11:53:31 -0500

    delete un-needed functions

--- github_getter.rb
+++ github_getter.rb
@@ -4,19 +4,16 @@ require 'json'
 
 class GithubGetter
 
-  BASE_URL = "https://api.github.com"
-
   SEARCH_URL = "https://api.github.com/search/users?q=location%3A%22new+york%22&sort=joined&order=asc"
 
-  def initialize (username, password, args = {})
+  def initialize (username, password)
     @username = username
     @password = password
-    @args = args
     @users = []
   end
 
-  def get_users
-    uri = URI(BASE_URL + "/users")
+  def ny_search
+    uri = URI(SEARCH_URL)
     req = Net::HTTP::Get.new(uri)
     req.basic_auth @username, @password
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
@@ -34,22 +31,6 @@ class GithubGetter
       }
     }
     @users
-  end
-
-  def filter_by_location(location)
-    @users.delete_if{|user|
-      !(user["location"].to_s.upcase.include?(location.upcase))
-    }
-    @users
-  end
-
-  def search_users
-    uri = URI(SEARCH_URL)
-    req = Net::HTTP::Get.new(uri)
-    req.basic_auth @username, @password
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
-      http.request(req).body
-    }
   end
 
 end
--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 require_relative 'github_getter'
 
 test = GithubGetter.new()
-dudes = JSON.parse(test.search_users)
+dudes = JSON.parse(test.ny_search)
 
 dudes["items"][0..10].each do |dude|
   p dude["login"]
