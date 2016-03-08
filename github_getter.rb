7b761cea574e7bbae9719c5e4c80f4792ec23c39
cgrad01 <cgrad01@gmail.com>
2016-03-08 16:32:45 -0500

    integrate repo getter (MVP)

--- github_getter.rb
+++ github_getter.rb
@@ -5,7 +5,7 @@ class GithubGetter
 
   BASE_URL = "https://api.github.com"
 
-  SEARCH_URL = "https://api.github.com/search/users?q=location%3A%22new+york%22&sort=joined&order=asc"
+  NY_SEARCH_URL = "https://api.github.com/search/users?q=location%3A%22new+york%22&sort=joined&order=asc"
 
   def initialize (username, password)
     @username = username
@@ -14,7 +14,7 @@ class GithubGetter
   end
 
   def ny_search
-    uri = URI(SEARCH_URL)
+    uri = URI(NY_SEARCH_URL)
     req = Net::HTTP::Get.new(uri)
     req.basic_auth @username, @password
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
@@ -34,7 +34,18 @@ class GithubGetter
     @users
   end
 
-  def get_repo_count
+  # experiment with query string to get the accurate repo response given time restraints
+  # this works for two users:
+  # https://api.github.com/search/repositories?q=+user:cgrad01+user:brynary+created:%3E2015-01-01T00:00:00z
+  def get_repo_counts(users)
+    users.each do |user|
+      uri = URI("https://api.github.com/search/repositories?q=+user:#{user.login}+created:%3E2015-01-01T00:00:00z")
+      req = Net::HTTP::Get.new(uri)
+      req.basic_auth @username, @password
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+        user.repo_count = JSON.parse(http.request(req).body)["total_count"]
+      }
+    end
   end
 
 end
--- user.rb
+++ user.rb
@@ -1,12 +1,13 @@
 class User
 
-  attr_accessor :login, :name, :location, :repo_count
+  attr_accessor :login, :name, :location, :repo_count, :repos
 
   def initialize(args = {})
     @login = args[:login]
     @name = args[:name]
     @location = args[:location]
     @repo_count = args[:repo_count]
+    @repos = []
   end
 
 end
