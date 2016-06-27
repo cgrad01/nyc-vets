fbf7f5a92f982d9c86527b7c3cfdf2417c8486cd
cgrad01 <cgrad01@gmail.com>
2016-06-27 17:57:45 -0400

    csv_writer to user_collection

--- user_collection.rb
+++ user_collection.rb
@@ -1,17 +1,19 @@
 require 'csv'
 require_relative 'user'
 
-class CSVWriter
+class UserCollection
+
+  attr_accessor :users
 
   HEADERS = ["login", "name", "location", "repo_count"]
 
   def initialize
+    @users = []
   end
 
-  def write(users)
-    CSV.open("output.csv", "wb") do |csv|
-      csv << HEADERS
-      users.each do |user|
+  def write_to_csv(file)
+    CSV.open(file, "w", write_headers: true, headers: HEADERS) do |csv|
+      @users.each do |user|
         csv << [user.login, user.name, user.location, user.repo_count]
       end
     end
